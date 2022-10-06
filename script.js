3a4b957e5aee8d22e8adac52456b9c16fe1ce0e2
root <root@LAPTOP-8EF6C2RE.localdomain>
2022-10-05 19:21:24 -0500

    one more

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+// get: "https://deckofcardsapi.com/api/deck/new/draw/?count=2"
 
-fetch("https://pokeapi.co/api/v2/pokemon/meowth")
-.then((response) => {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error("NETWORK RESPONSE ERROR");
-  }
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
+.then(res => {
+  return res.json();
 })
-.then(data => {
-  console.log(data);
-  displayPokemon(data)
+.then(res => {
+  console.log("success", res);
 })
-.catch((error) => console.error("FETCH ERROR:", error));
+.catch(err => {
+  console.log("error", err);
+})
+addEventListener('click', button)
+
+
+Add an event listener to your form so that when submitted, you trigger an AJAX call to the PokéAPI to find the Pokemon with the value from the text input field in the form. So if you type 52in the input, search for the pokemon with the ID of 52.
+Inside your .then()promise callback, handle the response and set the text of your <h1>to be the name of the Pokemon the user searched for.
+Hint: What does .preventDefault()do? Bonus: include an imgtag to your html and have a picture of the pokemon you search for appear on your page!
+// async function getText(file) {
+//   let x = await fetch(file);
+//   let y = await x.text();
+//   myDisplay(y);
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/meowth")
+// .then((response) => {
+//   if (response.ok) {
+//     return response.json();
+//   } else {
+//     throw new Error("Error");
+//   }
+// })
+// .then(data => {
+//   console.log(data);
+//   displayPokemon(data)
+//   const meowth = 
+// })
+// .catch((error) => console.error("Error", error));
 
 // const url = "http://pokeapi.salestock.net/api/v2/pokemon/7"
 // const url = "https://pokeapi.co/api/v2/pokemon/meowth";
